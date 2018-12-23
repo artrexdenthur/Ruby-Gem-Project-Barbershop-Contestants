91cb1edfcd549b6e6c75b4c40c400e14614e9d81
Paul Ashour <paul@Rachels-MacBook-Pro.local>
2018-12-22 20:13:12 -0700

    Better merge

--- lib/barbershop_contestants.rb
+++ lib/barbershop_contestants.rb
@@ -7,36 +7,6 @@ module BarbershopContestants
   # Your code goes here
 end
 
-<<<<<<< HEAD
-# doc = Scraper.scrape_quartet_champs
-# doc.each_with_index do |row, index|
-=======
-doc = Scraper.scrape_quartet_champs
-# binding.pry
-# doc.each do |row|
->>>>>>> ead516eaf0d53d65ac62ba7b7bc6a3e8d296e4d2
-#   # binding.pry
-#   row_data = row.text.split("\n")
-#   q_champs_hash = {
-#     year: row_data[1],
-#     name: row_data[2],
-#     score: row_data[3],
-#     district: row_data[4],
-#     comments: row_data[5],
-#     members: row_data[7],
-#     place: 1, # champions definitionally are first place
-#     type: "quartet"
-#   }
-#   Performance.create_q_champ(q_champs_hash)
-<<<<<<< HEAD
-#   # binding.pry
-#
-=======
-  # binding.pry
-
->>>>>>> ead516eaf0d53d65ac62ba7b7bc6a3e8d296e4d2
-# end
-
 # binding.pry
 CLI.start
 ### Remember this pattern: ###
