3d1c50d9ec86e26ccce493a768a79cf0656f67ac
Paul Ashour <paul@Rachels-MacBook-Pro.local>
2018-12-23 13:45:56 -0700

    Deleted old, out of position cli file. Got the scraper set up to work with the offlne file if nec.

--- lib/barbershop_contestants/scraper.rb
+++ lib/barbershop_contestants/scraper.rb
@@ -2,25 +2,50 @@
 # the requested site
 class Scraper
   QUARTET_CHAMPS_SITE = "https://www.barbershopwiki.com/wiki/BHS_International_Quartet_Champions"
+  LOCAL_SITES = {
+    quartet_champs: "./sites/BHS International Quartet Champions - Barbershop Wiki Project.html",
+    chorus_champs: "./sites/BHS International Chorus Champions - Barbershop Wiki Project.html",
+    chorus_2018: "./sites/BHS Intl Chorus Contest 2018 - Barbershop Wiki Project.html",
+    quartet_2018: "./sites/BHS Intl Quartet Contest 2018 - Barbershop Wiki Project.html"
+  }
+  CACHE_LOCATIONS = {
+    qchamps: "./sites/qchamps.txt",
+    cchamps: "./sites/cchamps.txt"
+  }
 
-  def self.scrape_HTML(page)
-    Nokogiri::HTML(open(page))
+  def self.scrape_or_load(page)
+    self.load_cache || Nokogiri::HTML(open(page))
   end
   # scraper should know what it's scraping,
   # but should not worry about the data classes'
   # architecture
 
-  def self.scrape_quartet_champs
-    doc = scrape_HTML(QUARTET_CHAMPS_SITE) # scrape site
-    binding.pry
+  def self.load_cache
+    # loaded = {}
+    # CACHE_LOCATIONS.each do |key, loc|
+    #   # load loc
+    #   # loaded[key] = fopen(loc)
+    # end
+    nil
+    # I'll have to figure this out later :/
     # try to get these to work:
     ##### doc = Nokogiri(string_or_io)
     ##### node.write_to(io, *options)
     ##### or
     ##### node.to_s / .to_html / .to_xml
+  end
+
+  def self.scrape_quartet_champs
+    doc = self.load_cache || self.scrape_or_load(LOCAL_SITES[:quartet_champs])
+    puts "Scraping local copy of site"
+    # TODO: reinstate real scraping functionality when in wifi
+    binding.pry
     doc = doc.css(".wikitable tbody tr") # get the champs table
     doc.delete(doc[0]) # get rid of the headers (can't figure out how to differentiate them with css)
-    doc.each do |row|
+  end
+
+  def self.scrape_and_create_quartet_champs
+    self.scrape_quartet_champs.each do |row|
       # binding.pry
       row_data = row.text.split("\n")
       q_champs_hash = {
--- lib/command_line_interface.rb
+++
@@ -1,9 +0,0 @@
-# requirements
-
-# class to drive the interface itself for barbershop_contestants
-class CommandLineInterface
-  def display
-    # use pretty printing a la:
-    # puts " category".colorize(:blue)
-  end
-end
